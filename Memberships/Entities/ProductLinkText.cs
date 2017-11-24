aeb0bbd74cec7ca6b85e2dffa5ddb7a826cf0eb8
Ricardo <danmleal@gmail.com>
2017-11-24 13:57:55 -0600

    Entities

---
+++ Memberships/Entities/ProductLinkText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Entities
+{
+    [Table("ProductLinkText")]
+    public class ProductLinkText
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(25)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Memberships.Entities;
 
 namespace Memberships.Models
 {
@@ -29,5 +30,13 @@ namespace Memberships.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductLinkText> ProductLinkTexts { get; set; }
     }
 }
